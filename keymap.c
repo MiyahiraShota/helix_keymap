55818846cc8a4ae626766dc7087f8cde307ba41e
shota.miyahira <shota.miyahira@stafes.com>
2020-01-30 13:20:52 +0900

    langをまとめると、誤作動が多かった

--- keymap.c
+++ keymap.c
@@ -46,8 +46,6 @@ enum macro_keycodes {
 #define M_SAMPLE M(KC_SAMPLEMACRO)
 #define GUI_SPC GUI_T(KC_SPC)
 #define CTL_SPC CTL_T(KC_SPC)
-#define L_EISU LT(_LOWER, KC_LANG2)
-#define R_KANA LT(_RAISE, KC_LANG1)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -61,15 +59,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Adjust|      | Alt  |      |      |L_EISU|GuiSpc|Space |R_KANA|      | Left | Down |  Up  |Right |
+   * |Adjust|      | Alt  |      | EISU |LOWER |GuiSpc|Space |RAISE | KANA | Left | Down |  Up  |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      ADJUST,  _______, KC_LALT, _______, _______, L_EISU,  GUI_SPC, KC_SPC,  R_KANA,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4, KC_5,                    KC_6,  KC_7, KC_8,    KC_9,    KC_0,    KC_GRV, \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R, KC_T,                    KC_Y,  KC_U, KC_I,    KC_O,    KC_P,    KC_BSPC, \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F, KC_G,                    KC_H,  KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V, KC_B,  KC_LBRC, KC_RBRC, KC_N,  KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+      ADJUST,  _______, KC_LALT, _______, EISU, LOWER, GUI_SPC, KC_SPC,  RAISE, KANA, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Lower
